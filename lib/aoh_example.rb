b9e27ee3f43cd87e9596d110e4441b7c5f7615f7
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-25 22:06:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,10 +20,10 @@ end
 
 def literal_aoh
   people = [
-    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-    JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-    KATE_G = { name:  "Kate Gompert", occupation: "None" }
+    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+    JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+    KATE_G = { name:  "Kate Gompert", occupation: "None" },
     BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
   # Using Array literal syntax only, build a nested array that uses the data in
